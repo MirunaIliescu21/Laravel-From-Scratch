50da3876d2443f0d3b8300a3d20b68d05d090c1e
MirunaIliescu21 <miruna.iliescu@gmail.com>
2026-08-19 14:12:55 +0200

    Initial commit - Laravel project setup

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Contact</title>
+</head>
+<body>
+    <h1>How can we help you ?</h1>
+
+    <div>
+{{--        <form action=""></form>--}}
+        <p>Placehorlder for the contact form</p>
+        <a href="/">Return home</a>
+
+    </div>
+
+</body>
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Hello, World!</h1>
+
+{{--    <div>--}}
+{{--        <a href="/about">About Us</a>--}}
+{{--    </div>--}}
+
+
+{{--    <div>--}}
+{{--        <a href="/contact">Contact us</a>--}}
+{{--    </div>--}}
+
+    <nav>
+        <a href="/">Home</a>
+        <a href="/about">About Us</a>
+        <a href="/contact">Contact us</a>
+    </nav>
+</body>
+</html>
