7ea7684279ee8129b26e8fffddbae54cc0fdda22
MirunaIliescu21 <miruna.iliescu@gmail.com>
2026-08-21 11:24:41 +0200

    DaisyUI Detour video - improving the user interface

--- resources/views/ideas/edit.blade.php
+++ resources/views/ideas/edit.blade.php
@@ -7,8 +7,8 @@
             <label for="description" class="block text-sm/6 font-medium text-white">Edit Your Idea</label>
             <div class="mt-2">
                 <textarea id="description" name="description" rows="3"
-                          class="block w-full rounded-md bg-gray-400 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6">
-                    {{ $idea->description }}
+                          class="textarea w-full">
+                          {{ $idea->description }}
                 </textarea>
 
                 <x-forms.error name="description" />
@@ -17,13 +17,13 @@
 
         <div class="mt-6 flex items-center gap-x-3">
             <button type="submit"
-                    class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+                    class="btn btn-primary">
                 Update
             </button>
 
             <button type="submit"
                     form="delete-idea-form"
-                    class="rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+                    class="btn btn-neutral">
                 Delete
             </button>
         </div>
